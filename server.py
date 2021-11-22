7ea157b87b0c86b7157948bec17debc9def5bdd1
SpyrosSiannasAccusonus <spyros.siannas@accusonus.com>
2021-11-22 17:49:18 +0200

    Add ip address to server argparse

--- server.py
+++ server.py
@@ -3,6 +3,6 @@ from src.argparser import ServerParser
 
 if __name__ == "__main__":
 	parser = ServerParser()
-	port, participant_number = parser.parse()
-	server = SantaServer("127.0.0.1", port, participant_number)
+	ip, port, participant_number = parser.parse()
+	server = SantaServer(ip, port, participant_number)
 	server.receive()
--- src/argparser.py
+++ src/argparser.py
@@ -7,10 +7,11 @@ class ServerParser:
 		self.parser = argparse.ArgumentParser(description="Host the Secret Santa assignment server")
 		self.parser.add_argument('-p','--port', type=int, default=cfg.port, help="The port in which the server should be setup")
 		self.parser.add_argument('-n', '--number', type=int, default=cfg.participant_number, help="The number of participants that will take place")
+		self.parser.add_argument('-ip','--addr', type=str, default=cfg.ip, help="The ip address of the Secret Santa host")
 
 	def parse(self) -> tuple:
 		args = self.parser.parse_args()
-		return args.port, args.number
+		return args.addr, args.port, args.number
 
 class ClientParser:
 	def __init__(self) -> None:
