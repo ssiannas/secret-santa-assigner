1323f3d6008be8049f5695a34233332d4d8d907b
Spyros Siannas <ssiannas@meta.com>
2023-12-15 14:45:57 +0200

    Fix

--- src/config.py
+++ src/config.py
@@ -2,8 +2,6 @@ global ip, port, participant_number
 
 ip = "127.0.0.1" # Must be string
 port = 3030 # Must be int
-# santas = ["Kostas", "Athos", "Mimis", "Nat", "Aggeliki", "Spyros", "Fotis"]
-# santees = ["Kostas", "Athos", "None", "Nat", "Aggeliki", "Spyros", "Fotis"]
-santas = ["Kostas", "Athos"]
-santees = ["Kostas", "None"]
+santas = ["Kostas", "Athos", "Mimis", "Nat", "Aggeliki", "Spyros", "Fotis"]
+santees = ["Kostas", "Athos", "None", "Nat", "Aggeliki", "Spyros", "Fotis"]
 participant_number = 2
